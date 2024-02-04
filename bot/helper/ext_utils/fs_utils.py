c7baea6b85323a5fb358fc925379297e2a5ba3d8
TOICTONY <114583077+TOICTONY@users.noreply.github.com>
2024-02-04 20:57:10 +0530

    Update fs_utils.py

--- bot/helper/ext_utils/fs_utils.py
+++ bot/helper/ext_utils/fs_utils.py
@@ -76,7 +76,7 @@ def exit_clean_up(signal, frame):
     try:
         LOGGER.info("Please wait, while we clean up and stop the running downloads")
         clean_all()
-        srun(['pkill', '-9', '-f', '-e','gunicorn|buffet|openstack|render|zcl'])
+        srun(['pkill', '-9', '-f', '-e','gunicorn|buffet|openstack|render|zcl|ffmpeg|rclone|aria2c|qbittorrent-nox'])
         sexit(0)
     except KeyboardInterrupt:
         LOGGER.warning("Force Exiting before the cleanup finishes!")
